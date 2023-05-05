768d4099076339675a6f10372157ca13ea9c3e8e
anitpeter8 <anitpeter3100@gmail.com>
2023-05-05 08:54:29 +0530

    student db

---
+++ backend/models/studentmodel.js
@@ -0,0 +1,68 @@
+const mongoose =require('mongoose');
+const Schema=mongoose.Schema;
+const studentSchema=new Schema({
+    fullname:{
+        type:String
+    },
+    
+    studentid:{
+        type:String
+    },
+    class:{
+        type:Number
+    },
+    semester:{
+        type:Number
+    },
+    branch:{
+        type:String
+    },
+    emailid:{
+        type:String
+    },
+    mobno:{
+        type:Number
+    },
+    address:{
+        type:String
+    },
+    street:{
+        type:String
+    },
+    city:{
+        type:String
+    },
+    district:{
+        type:String
+    },
+    pincode:{
+        type:Number
+    },
+    cgpa:{
+        type:Number
+    },
+    noofbacklogs:{
+        type:Number
+    },
+    historyofbacklogs:{
+        type:Boolean
+    },
+    cocubesscore:{
+        type:Number
+    },
+    amcatscore:{
+        type:Number
+    },
+    tenth:{
+        type:Number
+    },
+    twelth:{
+        type:Number
+    },
+    resumelink:{
+        type:String
+    }
+
+    
+    
+})
