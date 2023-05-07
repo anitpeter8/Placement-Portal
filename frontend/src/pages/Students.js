37ec81b6307763bb95c960e48c609217ad4d3048
anitpeter8 <anitpeter3100@gmail.com>
2023-05-07 16:25:57 +0530

    student page updates

--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
 
 const Students = () => {
+  const [students,setStudents]=useState(null);
+
+  useEffect(()=>{
+    axios.get('http://localhost:9000/students').then((res)=>{
+      setStudents(res.data);
+    }).catch((error)=>{
+      console.log(error.message);
+    })
+  })
   return (
+    <>
+  
     <div>Students</div>
+    {students && students.map((student)=>(
+      <>
+      <p>{student.fullname}</p>
+      <p>{student.branch}</p>
+      </>
+    ))}
+    </>
   )
 }
 export default Students;
