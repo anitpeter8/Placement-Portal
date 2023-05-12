f31d0cdd86c17e28a2a6e091cb95fc03ff9177e5
Anitta P Markose <anittapm1708@gmail.com>
2023-05-12 13:09:31 +0530

    minor updates

--- frontend/src/components/Announcement.js
+++ frontend/src/components/Announcement.js
@@ -1,18 +1,28 @@
 import EditAnnouncement from "../modals/EditAnnouncement";
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../css/Announcement.css';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/Announcement.css";
 import moment from "moment";
 const Announcement = ({ announcement }) => {
   return (
     <div className="single-card">
-      <Card style={{ borderRadius: '25px' }} className="card-content">
-        <Card.Header className="header" style={{ borderRadius: '25px' }}>
+      <Card style={{ borderRadius: "25px" }} className="card-content">
+        <Card.Header className="header" style={{ borderRadius: "25px" }}>
           <div className="d-flex justify-content-between">
-            <Card.Title><h5>{moment(announcement.createdAt).format('LL')}</h5></Card.Title>
-            <EditAnnouncement announcement={announcement} />
+            <Card.Title>
+              <h5>{moment(announcement.createdAt).format("LL")}</h5>
+            </Card.Title>
+            <div className="btns">
+              <EditAnnouncement announcement={announcement} />
+              <Button className="dlt-btn" onClick={{}}>
+                Delete
+              </Button>
+            </div>
           </div>
-          <Card.Title><h4>{announcement.heading}</h4></Card.Title>
+          <Card.Title>
+            <h4>{announcement.heading}</h4>
+          </Card.Title>
         </Card.Header>
         <Card.Body>
           <Card.Text>
@@ -21,6 +31,6 @@ const Announcement = ({ announcement }) => {
         </Card.Body>
       </Card>
     </div>
-  )
-}
+  );
+};
 export default Announcement;
