f1e4ebae5375b59d6c32b0623df8fcc18c4f78d2
anitpeter8 <anitpeter3100@gmail.com>
2023-05-23 16:18:06 +0530

    different put methods

--- backend/routes/statistics.js
+++ backend/routes/statistics.js
@@ -11,6 +11,71 @@ routes.post('/',async(req,res)=>{
         res.status(400).json({error:error.message});
     }
 })
+routes.put('/delete/:yearid/:offerid',async(req,res)=>{
+    const yearid=req.params.yearid;
+    const offerid=req.params.offerid;
+    console.log(yearid,offerid);
+    try{
+      const stat= await Statistics.findByIdAndUpdate({_id:yearid},
+        {
+            $pull:{offers:{_id:offerid}}
+        })
+        //res.json({mssg:"done"});
+        res.json(stat);
+        //console.log(stat);
+    }
+    
+
+    catch(error)
+    {
+        res.status(400).json({error:error.message});
+    }
+})
+
+routes.put('/edit/:yearid/:offerid',async(req,res)=>{
+    const yearid=req.params.yearid;
+    const offerid=req.params.offerid;
+    console.log(yearid,offerid);
+    try{
+      const stat= await Statistics.findByIdAndUpdate({_id:yearid},
+        {
+            $set:{offers:{_id:offerid,...req.body}}
+        })
+        //res.json({mssg:"done"});
+        res.json(stat);
+        //console.log(stat);
+    }
+    
+
+    catch(error)
+    {
+        res.status(400).json({error:error.message});
+    }
+})
+
+
+
+routes.put('/add/:yearid',async(req,res)=>{
+    const yearid=req.params.yearid;
+    const offerid=req.params.offerid;
+    console.log(yearid);
+    try{
+      const stat= await Statistics.findByIdAndUpdate({_id:yearid},
+        {
+            $push:{offers:req.body}
+        })
+        //res.json({mssg:"done"});
+        res.json(stat);
+        //console.log(stat);
+    }
+    
+
+    catch(error)
+    {
+        res.status(400).json({error:error.message});
+    }
+})
+
 routes.get('/',async(req,res)=>{
     try{
         const all=await Statistics.find({});
