f26c0c640e3c015f1738e5b29c046e31e3969f30
MAAdithya <adithyama65.com>
2023-05-25 14:23:35 +0530

    minor update in student

--- frontend/src/components/Student.js
+++ frontend/src/components/Student.js
@@ -4,23 +4,23 @@ import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/Student.css';
 //import moment from "moment";
-const Student = ({student }) => {
+const Student = ({ student }) => {
 
   return (
-    
-
     <div className="single-card">
       <Card className="stbody">
-      
+
         <Card.Body>
           <Card.Text>
             <div className="edit">
-          <div>
-          <h4>{student.fullname}</h4>
-          </div>
-          <div className="edit-modal">
-            <EditStudent student={student} /></div>
-          </div>
+              <div>
+                <h6>{student.studentid}</h6>
+                <h6>S{student.semester}</h6>
+                <h4>{student.fullname}</h4>
+              </div>
+              <div className="edit-modal">
+                <EditStudent student={student} /></div>
+            </div>
             <p>{student.branch}</p>
           </Card.Text>
         </Card.Body>
