4d5df73921919a89e86788d584f23052396ea8fe
adithya-ma <adithyama7@gmail.com>
2023-06-26 18:28:19 +0530

    Statistics page

--- frontend/src/pages/admin/Statistics.js
+++ frontend/src/pages/admin/Statistics.js
@@ -424,9 +424,13 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import "../../css/StatisticsPage.css";
 import NewOffer from '../../modals/NewOffer';
+import "../../modals/ViewAppliedStudents.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 
 import Button from 'react-bootstrap/Button';
 import EditOffer from '../../modals/EditOffer';
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 const Statistics = () => {
 
@@ -434,7 +438,7 @@ const Statistics = () => {
 
   const [EditOffermodal, setEditOffermodal] = useState(false);
 
-  const [prefilldata,setprefilldata]=useState({companyname:"loading",year:0,CSE:0,CIVIL:0,EEE:0,ECE:0,MECH:0,AI:0});
+  const [prefilldata, setprefilldata] = useState({ companyname: "loading", year: 0, CSE: 0, CIVIL: 0, EEE: 0, ECE: 0, MECH: 0, AI: 0 });
   const [stats, setstats] = useState();
   const [Search, setSearch] = useState('')
   const [year, setyear] = useState('ALL');
@@ -454,97 +458,124 @@ const Statistics = () => {
     <div className='statastics'>
       <div className="stathead">
         <h1 className="statheading">STATISTICS</h1>
+        <div className="new-announcement-btn">
 
-      </div>
-      <div className="new-announcement-btn">
-          <Button variant="primary"
+          <Button variant="primary" className='newOffer'
             style={{ backgroundColor: '#ffa8a2', height: '45px', color: '#660a0a' }}
             onClick={() => setNewOffermodal(true)}>
             + New Offer
           </Button>
           <NewOffer onclose={() => setNewOffermodal(false)} NewOffermodal={NewOffermodal} />
-          <EditOffer onclose={() =>{
-setEditOffermodal(false)
-setprefilldata()
-          } } EditOffermodal={EditOffermodal} prefilldata={prefilldata} />
-
+          <EditOffer onclose={() => {
+            setEditOffermodal(false)
+            setprefilldata()
+          }} EditOffermodal={EditOffermodal} prefilldata={prefilldata} />
 
         </div>
-      <div>
-        <input placeholder='search' onChange={(e) => {
-          setSearch(e.target.value)
-          console.log(e.target.value)
-        }
-        } />
-        <select onChange={(e) => {
-          setyear(e.target.value)
-          console.log(e.target.value)
-        }}>
-          <option value='ALL' selected >ALL</option>
-          <option value={2023}>2023</option>
-          <option value={2022}>2022</option>
-          <option value={2021}>2021</option>
-          <option value={2020}>2020</option>
-        </select>
       </div>
+        <div className='stat-tablemain'>
 
-      <div className='stat-table'>
-        <table>
-          <thead>
-            <th style={{ width: "20em" }}>Recruiter</th>
-            <th style={{ width: "15em" }}>Year of Recruitment</th>
-            <th>Package</th>
-            <th style={{ width: "10em" }}>CSE</th>
-            <th style={{ width: "10em" }}>AI</th>
-            <th style={{ width: "10em" }}>MECH</th>
-            <th style={{ width: "10em" }}>CIVIL</th>
-            <th style={{ width: "10em" }}> EEE</th>
-            <th style={{ width: "10em" }}>ECE</th>
-            <th>Total Recruits</th>
-            <th>Edit</th>
-          </thead>
-          <tbody>
-            {stats && stats.filter((offer) => {
-              console.log(offer.year)
-              console.log(year)
-              return year === 'ALL' ? offer :
-                offer.year == year
-            }).filter((offer) => {
-              return Search === '' ? offer :
-                offer.companyname.toLowerCase().includes(Search);
-            }).map((offer) =>
+          <div className='filter-details'>
+            <input placeholder='Search Recruiter' className='input-recruiter' onChange={(e) => {
+              setSearch(e.target.value)
+              console.log(e.target.value)
+            }
+            } />
+            <div>
+              <select className='input-year' onChange={(e) => {
+                setyear(e.target.value)
+                console.log(e.target.value)
+              }}>
+                <option value='Filter by Year' selected >Filter by Year</option>
+                <option value={2023}>2023</option>
+                <option value={2022}>2022</option>
+                <option value={2021}>2021</option>
+                <option value={2020}>2020</option>
+              </select>
+            </div>
+          </div>
 
-            (
+          <div className='stat-table'>
+            <table className='applied-table'>
+              <thead>
+                <th style={{ width: "20em" }}>Recruiter</th>
+                <th style={{ width: "20em" }}>Year of Recruitment</th>
+                <th style={{ width: "10em" }}>Package</th>
+                <th style={{ width: "10em" }}>CSE</th>
+                <th style={{ width: "10em" }}>AI</th>
+                <th style={{ width: "10em" }}>MECH</th>
+                <th style={{ width: "10em" }}>CIVIL</th>
+                <th style={{ width: "10em" }}> EEE</th>
+                <th style={{ width: "10em" }}>ECE</th>
+                <th style={{ width: "10em" }}>Total Recruits</th>
+                <th style={{ width: "1em" }}>Edit</th>
+                <th style={{ width: "10em" }}></th>
+              </thead>
+              <tbody>
+                {stats && stats.filter((offer) => {
+                  console.log(offer.year)
+                  console.log(year)
+                  return year === 'ALL' ? offer :
+                    offer.year == year
+                }).filter((offer) => {
+                  return Search === '' ? offer :
+                    offer.companyname.toLowerCase().includes(Search);
+                }).map((offer) =>
+
+                (
 
 
-              <tr>
-                <td>{offer.companyname}</td>
+                  <tr>
+                    <td>{offer.companyname}</td>
 
-                <td>{offer.year}</td>
-                <td>{offer.LPA}</td>
-                <td>{offer.CSE}</td>
-                <td>{offer.AI}</td>
-                <td>{offer.MECH}</td>
-                <td>{offer.CIVIL}</td>
-                <td>{offer.EEE}</td>
-                <td>{offer.ECE}</td>
-                <td>{offer.total}</td>
-                <td><button onClick={()=>{
-                  console.log(offer._id);
-                  setprefilldata(offer);
-                  setEditOffermodal(true);
-                }}>Edit</button></td>
-              </tr>
-            )
+                    <td>{offer.year}</td>
+                    <td>{offer.LPA}</td>
+                    <td>{offer.CSE}</td>
+                    <td>{offer.AI}</td>
+                    <td>{offer.MECH}</td>
+                    <td>{offer.CIVIL}</td>
+                    <td>{offer.EEE}</td>
+                    <td>{offer.ECE}</td>
+                    <td>{offer.total}</td>
+                    <td>
+                      <Button
+                        title="Edit Job"
+                        variant="secondary"
 
-            )}
-          </tbody>
-        </table>
+
+                        onClick={() => {
+                          console.log(offer._id);
+                          setprefilldata(offer);
+                          setEditOffermodal(true);
+                        }}
+
+                      >
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                      </Button>
+                    </td>
+
+                    <td>
+
+                      <Button
+                        title="Delete Job"
+                        variant="danger"
+                        className="dlt-btn"
+
+                      >
+                        <FontAwesomeIcon icon={faTrashCan} />
+                      </Button>
+                    </td>
+                  </tr>
+                )
+
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-
-    </div>
-  )
+      )
 }
 
 
-export default Statistics;
+      export default Statistics;
